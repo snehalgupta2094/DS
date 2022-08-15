3a1b25fe94363bfd0948077da3453b412fc18543
guptasn <guptasn@vmware.com>
2022-08-15 12:27:52 +0530

    heap

---
+++ src/Searching/LinearSearchRecursive.java
@@ -0,0 +1,16 @@
+package Searching;
+
+public class LinearSearchRecursive {
+
+    public static boolean search(int arr[],int i, int key){
+        if(i==arr.length)
+            return false;
+        if(arr[i]==key)
+            return true;
+        return search(arr,i+1,key);
+    }
+    public static void main(String[] args) {
+       int arr[]={2,1,3,4,6,7};
+        System.out.println(search(arr,0,9));
+    }
+}
