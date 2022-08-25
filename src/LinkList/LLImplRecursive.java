02981398c4b24c877c9e05da77aef875d2a5c061
guptasn <guptasn@vmware.com>
2022-08-25 14:16:46 +0530

    Recursive LL

--- src/LinkList/LLImplRecursive.java
+++ src/LinkList/LLImplRecursive.java
@@ -11,7 +11,7 @@ class ListNode{
 
 
 public class LLImplRecursive {
-    //Reverse Linked list
+    //Reverse
     public static ListNode reverse(ListNode head){
         //base-In the end we need to make last node as head so it should return last node to newNode
         if(head==null || head.next==null)
@@ -20,24 +20,15 @@ public class LLImplRecursive {
         ListNode newNode=reverse(head.next);
         head.next.next=head;
         head.next=null;
-
         return newNode;
     }
-    //Insert at tail
-
-    //Insert at head
-
-    //Insert in middle
-
-    //Delete at tail
-
-    //Delete in middle
-    public static ListNode deleteInMiddle(ListNode head, int val){
+    //Delete
+    public static ListNode delete(ListNode head, int val){
         //base
         if(head==null)
             return null;
         //head recursion
-        ListNode post=deleteInMiddle(head.next,val);
+        ListNode post=delete(head.next,val);
 
         if(head.val!=val){
             head.next=post;
@@ -45,9 +36,6 @@ public class LLImplRecursive {
         }
         return post;
     }
-
-    //Delete at head
-
     //Print
     public static void printLL(ListNode head){
         //base
@@ -56,7 +44,20 @@ public class LLImplRecursive {
         System.out.println(head.val);
         printLL(head.next);
     }
-
+    //Insert
+    private static ListNode insert(ListNode head, ListNode newNode) {
+        //base
+        if(head==null) {
+            head=newNode;
+            return head;
+        }
+        if(head.next==null){
+            head.next=newNode;
+            return head;
+        }
+        insert(head.next,newNode);
+        return head;
+    }
     public static void main(String[] args) {
         ListNode n6=new ListNode(6,null);
         ListNode n5=new ListNode(5,n6);
@@ -70,12 +71,16 @@ public class LLImplRecursive {
         printLL(head);
         //delete
         System.out.println("---Deletion---");
-        head=deleteInMiddle(head,6);
+        head=delete(head,6);
         printLL(head);
-
-        //delete
+        //reverse
         System.out.println("---Reverse---");
         head=reverse(head);
+        printLL(head);
+        //insert
+        System.out.println("---Insertion---");
+        ListNode newNode=new ListNode(7,null);
+        head=insert(head,newNode);
         printLL(head);
     }
 }
