9e6d1600e9a2546a02e1eb87fe8a947364c72b15
guptasn <guptasn@vmware.com>
2022-09-01 13:30:17 +0530

    infix

--- src/Stacks/EvaluateInfix.java
+++ src/Stacks/EvaluateInfix.java
@@ -17,20 +17,22 @@ public class EvaluateInfix {
         }
         return -1;
     }
-    public static int calculate(int b, int a, char op){
+    public static int calculate(int a, int b, char op){
         switch (op){
             case '+':
                 return a+b;
             case '-':
-                return a-b;
+                return b-a;
             case '/':
-                return a/b;
+                if (a == 0)
+                    throw new UnsupportedOperationException("Cannot divide by zero");
+                return b/a;
             case '^':
                 return a^b;
             case '*':
                 return a*b;
         }
-        return -1;
+        return 0;
     }
     public static int solve(String expr){
         Stack<Integer> operand=new Stack<>();
@@ -40,9 +42,19 @@ public class EvaluateInfix {
             char ch=expr.charAt(i);
 
             //operand
-            if(ch>='0' && ch<='9'){
-                int num=ch-'0';
-                System.out.println(num);
+            if(Character.isDigit(ch)){
+                //Entry is Digit, it could be greater than one digit number
+                int num = 0;
+                while (Character.isDigit(ch)) {
+                    num = num*10 + (ch-'0');
+                    i++;
+                    if(i < expr.length())
+                        ch = expr.charAt(i);
+                    else
+                        break;
+                }
+                i--;
+                //push it into stack
                 operand.push(num);
             }
             //open bracket
@@ -51,7 +63,7 @@ public class EvaluateInfix {
             }
             //close bracket
             else if(ch==')'){
-                while (!operator.isEmpty() && operator.peek()!='('){
+                while (operator.peek()!='('){
                     //push it back to stack
                     int result=calculate(operand.pop(),operand.pop(), operator.pop());
                     operand.push(result);
@@ -59,23 +71,31 @@ public class EvaluateInfix {
                 operator.pop();
             }
             //operator
-            else{
+            else if(isOperator(ch)){
                 //1. If current operator has higher precedence than operator on top of the stack,
                 //the current operator can be placed in stack
                 // 2. else keep popping operator from stack and perform the operation in numbers stack till
                 //either stack is not empty or current operator has higher precedence than operator on top of the stack
                 while(!operator.isEmpty() && preecedence(ch)<=preecedence(operator.peek())){
                     int output=calculate(operand.pop(),operand.pop(), operator.pop());
+                    System.out.println("---"+output);
                    operand.push(output);
                 }
                 operator.push(ch);
             }
         }
-
+        while(!operator.isEmpty()){
+            int output=calculate(operand.pop(),operand.pop(), operator.pop());
+            System.out.println("---"+output);
+            operand.push(output);
+        }
         return operand.pop();
     }
+    public static boolean isOperator(char c){
+        return (c=='+'||c=='-'||c=='/'||c=='*'||c=='^');
+    }
     public static void main(String[] args) {
-        String infix="(5+3)/4";
-        System.out.println(solve(infix));
+        String infixExpression = "2*(5*(3+6))/15-2";
+        System.out.println(solve(infixExpression));
     }
 }
