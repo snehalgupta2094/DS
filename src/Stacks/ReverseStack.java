39cbba214c7b3ce68a952474f3864904f327590c
guptasn <guptasn@vmware.com>
2022-09-03 10:42:38 +0530

    Reverse stack

--- src/Stacks/ReverseStack.java
+++ src/Stacks/ReverseStack.java
@@ -3,38 +3,40 @@ package Stacks;
 import java.util.Stack;
 
 public class ReverseStack {
-public static void main(String[] args) {
-	Stack<Integer> s=new Stack<Integer>();
-	s.push(1);
-	s.push(2);
-	s.push(3);
-	s.push(4);
-	s.push(5);
-	System.out.println(s);
-	reverse(s);
-	System.out.println(s);
-}
 
-private static void reverse(Stack<Integer> s) {
-	int element=s.pop();
-	if(s.size()!=1)
-	{
+	public static void main(String[] args) {
+		Stack<Integer> s = new Stack<Integer>();
+		s.push(1);
+		s.push(2);
+		s.push(3);
+		s.push(4);
+		s.push(5);
+		System.out.println(s);
 		reverse(s);
+		System.out.println(s);
 	}
-	
-	pushToBottom(element,s);
-}
 
-private static void pushToBottom(int element, Stack<Integer> s) {
-	// TODO Auto-generated method stub
-	if(s.isEmpty())
-	{
-		s.push(element);
-		return;
+
+	private static void reverse(Stack<Integer> s) {
+		//base case
+		if (s.isEmpty()) {
+			return;
+		}
+
+		int element = s.pop();
+		reverse(s);
+		insertAtBottom(s, element);
 	}
-	
-int temp=s.pop();
-pushToBottom(element, s);
-s.push(temp);
-}
+
+	public static void insertAtBottom(Stack<Integer> stack, int element) {
+		//base case
+		if (stack.isEmpty()) {
+			stack.push(element);
+			return;
+		}
+		int ele = stack.pop();
+		//recursive call
+		insertAtBottom(stack, element);
+		stack.push(ele);
+	}
 }
