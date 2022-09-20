36cf48f84a9568215993d909d620d82b58c91d7c
guptasn <guptasn@vmware.com>
2022-09-20 19:28:43 +0530

    CYCLE

--- src/Graphs/cycleDetection/DetectCycleDirected.java
+++ src/Graphs/cycleDetection/DetectCycleDirected.java
@@ -1,4 +1,4 @@
-package com.practice.Graphs.cycleDetection;
+package Graphs.cycleDetection;
 
 import java.util.ArrayList;
 
@@ -15,20 +15,37 @@ class DetectCycleDirectedImpl{
         adj.get(u).add(v);
     }
 
-    public boolean[] isCycleExist() {
-        boolean visited[]=new boolean[v];
-        boolean recStack[]=new boolean[v];
-        for(int i=0;i<v;i++)
-        {
-            if(!visited[i])
-                if(dfs(i,visited,recStack))
-                {
-                    return recStack;
-                }
-
+    public boolean detectCycle_DFS(boolean[] visited,boolean dfsVisited[],int src){
+        visited[src]=true;
+        dfsVisited[src]=true;
+        for(int neighbor: adj.get(src)){
+            if(!visited[neighbor])
+            {
+                if(detectCycle_DFS(visited,dfsVisited,neighbor))
+                    return true;
+            }
+            else if(dfsVisited[neighbor])
+            {
+                return true;
+            }
         }
-        return recStack;
+        dfsVisited[src]=false;
+        return false;
+
     }
+    public boolean detectCycle_DFS(){
+        boolean visited[] =new boolean[v];
+        boolean dfsVisited[]=new boolean[v];
+        for(int i=0;i<v;i++){
+            if(!visited[i]){
+                if(detectCycle_DFS(visited,dfsVisited,i)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
     public boolean dfs(int u, boolean visited[], boolean recStack[]){
         visited[u]=true;
         recStack[u]=true;
@@ -43,32 +60,23 @@ class DetectCycleDirectedImpl{
         recStack[u]=false;
         return false;
     }
-
-    public void printCycle(){
-        boolean[] res=isCycleExist();
-        for (int i = 0; i < res.length; i++) {
-            if(res[i])
-                System.out.println(i);
-        }
-    }
 }
 public class DetectCycleDirected {
     public static void main(String[] args) {
-/*        DetectCycleDirectedImpl g=new DetectCycleDirectedImpl(6);
+      DetectCycleDirectedImpl g=new DetectCycleDirectedImpl(6);
         g.addEdge(0,1);
-        g.addEdge(2,0);
         g.addEdge(1,2);
         g.addEdge(3,0);
         g.addEdge(3,4);
         g.addEdge(4,5);
-        g.printCycle();*/
-        DetectCycleDirectedImpl g=new DetectCycleDirectedImpl(5);
+        System.out.println(g.detectCycle_DFS());
+     /*   DetectCycleDirectedImpl g=new DetectCycleDirectedImpl(5);
         g.addEdge(0,1);
         g.addEdge(2,1);
         g.addEdge(4,0);
         g.addEdge(4,2);
         g.addEdge(2,3);
         g.addEdge(3,4);
-        g.printCycle();
+        System.out.println(g.detectCycle_DFS());*/
     }
 }
