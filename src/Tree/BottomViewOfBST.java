4202560f3b053599ab13fb6ac8343dbcd77bbb3b
guptasn <guptasn@vmware.com>
2022-09-25 14:34:50 +0530

    Traversal

--- src/Tree/BottomViewOfBST.java
+++ src/Tree/BottomViewOfBST.java
@@ -1,102 +1,95 @@
 package Tree;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Map;
+import java.util.Queue;
 import java.util.TreeMap;
-import java.util.stream.Stream;
 
-public class BottomViewOfBST 
-{
-	class TreeNode
+
+
+public class BottomViewOfBST {
+
+	class TNode
 	{
+		TNode left;
+		TNode right;
 		int data;
-		TreeNode left;
-		TreeNode right;
-		TreeNode(int data)
+		TNode(int data)
 		{
 			this.data=data;
 		}
 	}
-	TreeNode root;
-	class QueueEntry
-	{
-		TreeNode node;
-		int distance;
-		QueueEntry(TreeNode node, int distance)
-		{
-			this.node=node;
-			this.distance=distance;
+	TNode root;
+	class Pair<K,V>{
+		K first;
+		V second;
+		Pair(K first, V second){
+			this.first=first;
+			this.second=second;
 		}
 	}
-	public TreeNode createTree()
+	public void bottomView()
 	{
-		this.root=new TreeNode(1);
-		TreeNode n1=new TreeNode(2);
-		TreeNode n2=new TreeNode(3);
-		TreeNode n3=new TreeNode(4);
-		TreeNode n4=new TreeNode(5);
-		TreeNode n5=new TreeNode(6);
-		TreeNode n6=new TreeNode(7);
-		TreeNode n7=new TreeNode(8);
-		TreeNode n8=new TreeNode(9);
-		TreeNode n9=new TreeNode(10);
-		TreeNode n10=new TreeNode(11);
-		
-		root.left  = n1;
-        root.right = n2;
-         
-        n1.left  = n3;
-        n1.right = n4;
-   
-        n2.left  = n5;
-        n2.right = n6;
-         
-        n3.right = n7;
-         
-        n4.left = n8;
-         
-        n5.right = n9;
-         
-        n6.right = n10;
-		
-        return root;
-	}
-	
-	private void bottomUpView() 
-	{
-		Map<Integer,QueueEntry> map=new TreeMap<Integer, QueueEntry>();
-		LinkedList<QueueEntry> queue=new LinkedList<QueueEntry>();
-		queue.add(new QueueEntry(root, 0));
-		
-		while(!queue.isEmpty())
-		{
-			QueueEntry tmp=queue.remove();
-			
-			if(tmp.node!=null)
-			{
-				map.put(tmp.distance, tmp);
-			if(tmp.node.left!=null)
-			{
-				queue.add(new QueueEntry(tmp.node.left, tmp.distance-1));
-				
-			}
-		    if(tmp.node.right!=null)
-		    {
-		    	queue.add(new QueueEntry(tmp.node.right, tmp.distance+1));
-		    }
-			}
-		}
-		
-		System.out.println();
-		map.forEach((k,v)->System.out.print(v.node.data+" "));
-		System.out.println("--trial--"+map.size());
-	}
-	
-	public static void main(String[] args) 
-	{
-		BottomViewOfBST tree=new BottomViewOfBST();
-		tree.createTree();
-		tree.bottomUpView();
-	}
-}
+		TreeMap<Integer,Integer> map=new TreeMap<>();
+		Queue<Pair<TNode,Integer>> queue=new ArrayDeque<>();
+		queue.offer(new Pair<>(root,0));
 
+		while (!queue.isEmpty()){
+			Pair<TNode,Integer> p=queue.poll();
+			TNode frontNode=p.first;
+			int hd=p.second;
+			map.put(hd,frontNode.data);
+
+			if(frontNode.left!=null)
+				queue.offer(new Pair<>(frontNode.left,hd-1));
+			if(frontNode.right!=null)
+				queue.offer(new Pair<>(frontNode.right,hd+1));
+		}
+		ArrayList<Integer> ans=new ArrayList<>();
+		for(int val:map.values()){
+			ans.add(val);
+		}
+		System.out.println(ans);
+	}
+
+	public TNode create()
+	{
+		this.root=new TNode(1);
+		TNode n1=new TNode(2);
+		TNode n2=new TNode(3);
+		TNode n3=new TNode(4);
+		TNode n4=new TNode(5);
+		TNode n5=new TNode(6);
+		TNode n6=new TNode(7);
+		TNode n7=new TNode(8);
+		TNode n8=new TNode(9);
+		TNode n9=new TNode(10);
+		TNode n10=new TNode(11);
+
+		root.left  = n1;
+		root.right = n2;
+
+		n1.left  = n3;
+		n1.right = n4;
+
+		n2.left  = n5;
+		n2.right = n6;
+
+		n3.left = n7;
+		n3.right = n8;
+
+		n4.left = n9;
+		n4.right = n10;
+
+		return root;
+	}
+
+	public static void main(String[] args) {
+		BottomViewOfBST tree=new BottomViewOfBST();
+		tree.create();
+		tree.bottomView();
+	}
+
+}
