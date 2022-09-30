2e02c48a79614f67b5bc855efaf77aea3ea0bf84
guptasn <guptasn@vmware.com>
2022-09-30 16:31:07 +0530

    Morris Traversal

--- src/Graphs/shortestPath/Dijkstra.java
+++ src/Graphs/shortestPath/Dijkstra.java
@@ -1,34 +1,40 @@
 package Graphs.shortestPath;
+
+import java.util.Arrays;
+
 class DijkstraImpl{
-    int v;
-    public void dijkstra(int graph[][]){
-        int value[]=new int[v];
-        boolean processed[]=new boolean[v];
-        int parent[]=new int[v];
-        for(int i=0;i<v;i++){
-            parent[i]=-1;
-            value[i]= Integer.MAX_VALUE;
+
+    public int[] dijkstra(int graph[][], int src){
+        int count=graph.length;
+        int distance[]=new int[count];
+        boolean visited[]=new boolean[count];
+
+        for(int i=0;i<count;i++){
+            distance[i]= Integer.MAX_VALUE;
         }
-        parent[0]=-1;
-        value[0]=0;
-        for(int i=0;i<v-1;i++){
-            int u=selectMinVertex(value,processed);
-            processed[u]=true;
-            for(int j:graph[i]){
-                if(graph[u][j]!=0 && !processed[j] && value[u]!=Integer.MAX_VALUE &&(value[u]+graph[u][j]<value[j]))
+        distance[src]=0;
+        for(int i=0;i<count;i++){
+
+            //update distance between neighbor and
+            int topNode=selectMinVertex(distance,visited);
+            visited[topNode]=true;
+
+            //update all neighbor vertex
+            for(int neighbor=0;neighbor<count;neighbor++){
+                if(graph[topNode][neighbor]!=0 && !visited[neighbor] && distance[topNode]!=Integer.MAX_VALUE &&(distance[topNode]+graph[topNode][neighbor]<distance[neighbor]))
                 {
-                    value[j]=value[u]+graph[u][j];
-                    parent[j]=u;
+                    distance[neighbor]=distance[topNode]+graph[topNode][neighbor];
                 }
             }
         }
+       return distance;
     }
-    public int selectMinVertex(int value[], boolean processed[]){
+    public int selectMinVertex(int distance[], boolean visited[]){
         int min=Integer.MAX_VALUE;
-        int vertex=0;
-        for(int i=0;i<v;i++){
-            if(!processed[i] && value[i]<min){
-                min=value[i];
+        int vertex=-1;
+        for(int i=0;i<distance.length;i++){
+            if(!visited[i] && distance[i]<min){
+                min=distance[i];
                 vertex=i;
             }
         }
@@ -37,6 +43,17 @@ class DijkstraImpl{
 }
 public class Dijkstra {
     public static void main(String[] args) {
-
+        DijkstraImpl d=new DijkstraImpl();
+        int graph[][] = new int[][] { { 0, 4, 0, 0, 0, 0, 0, 8, 0 },
+                                      { 4, 0, 8, 0, 0, 0, 0, 11, 0 },
+                                      { 0, 8, 0, 7, 0, 4, 0, 0, 2 },
+                                      { 0, 0, 7, 0, 9, 14, 0, 0, 0 },
+                                      { 0, 0, 0, 9, 0, 10, 0, 0, 0 },
+                                      { 0, 0, 4, 14, 10, 0, 2, 0, 0 },
+                                      { 0, 0, 0, 0, 0, 2, 0, 1, 6 },
+                                      { 8, 11, 0, 0, 0, 0, 1, 0, 7 },
+                                      { 0, 0, 2, 0, 0, 0, 6, 7, 0 } };
+        int[] distance=d.dijkstra(graph,0);
+        System.out.println(Arrays.toString(distance));
     }
 }
