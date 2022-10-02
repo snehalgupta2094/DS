a442b2800ed74a27d232b6332421785d315066e3
guptasn <guptasn@vmware.com>
2022-10-02 13:14:23 +0530

    Prims

--- src/Graphs/stronglyConnected/Kosaraju.java
+++ src/Graphs/stronglyConnected/Kosaraju.java
@@ -7,75 +7,89 @@ class KosarajuImpl {
 
     int v;
     ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
-    ArrayList<ArrayList<Integer>> rev = new ArrayList<ArrayList<Integer>>();
+    ArrayList<ArrayList<Integer>> transpose = new ArrayList<ArrayList<Integer>>();
 
     KosarajuImpl(int v) {
         this.v = v;
         for (int i = 0; i < v; i++){
             adj.add(new ArrayList<>());
-            rev.add(new ArrayList<>());
+            transpose.add(new ArrayList<>());
         }
     }
+
     public void addEdge(int u, int v) {
         adj.get(u).add(v);
     }
+    public void dfs(int node, boolean visited[],Stack<Integer> stack){
+        visited[node]=true;
+        for(int neighbor: adj.get(node)){
+            if(!visited[neighbor]){
+                dfs(neighbor,visited,stack);
+            }
+        }
+        //topo logic
+        stack.push(node);
 
-    public void reverse(){
-        for(int i=0;i<v;i++)
-            for(int j: adj.get(i))
-                rev.get(j).add(i);
-    }
-    public void DFS1(int u, boolean visited[], Stack<Integer> stack){
-        visited[u]=true;
-        for(int v: adj.get(u)) {
-            if(!visited[v])
-                DFS1(v,visited,stack);
-        }
-        stack.push(u);
-    }
-    public void DFS2(int u, boolean visited[]){
-        System.out.print(u+" ");
-        visited[u]=true;
-        for(int v: rev.get(u)) {
-            if(!visited[v])
-                DFS2(v,visited);
-        }
     }
     public void findSCC(){
+
+        //topo sort
      Stack<Integer> stack=new Stack<>();
      boolean visited[]=new boolean[v];
 
      for(int i=0;i<v;i++)
          if(!visited[i])
-             DFS1(i,visited,stack);
+             dfs(i,visited,stack);
 
-     reverse();
-
-     for(int i=0;i<v;i++)
+         //transpose
+     for(int i=0;i<v;i++){
          visited[i]=false;
-
-
-     while(!stack.isEmpty()){
-         int curr=stack.pop();
-         if(!visited[curr]){
-             System.out.println("SCC are:");
-             DFS2(curr,visited);
-             System.out.println();
+         for(int neighbor: adj.get(i))
+         {
+             transpose.get(neighbor).add(i);
          }
      }
+
+    //dfs
+        int count=0;
+     while(!stack.isEmpty()){
+         int top=stack.pop();
+         if(!visited[top]){
+             System.out.println("---SCC---");
+             revDfs(top,visited);
+             System.out.println();
+             count++;
+         }
+     }
+        System.out.println("count:"+count);
+    }
+    public void revDfs(int node,boolean[] visited){
+        System.out.print(node+" ");
+        visited[node]=true;
+        for(int neighbor: transpose.get(node)){
+            if(!visited[neighbor])
+                revDfs(neighbor,visited);
+        }
     }
 }
+
 public class Kosaraju {
 
     public static void main(String[] args) {
-        KosarajuImpl g=new KosarajuImpl(6);
+/*        KosarajuImpl g=new KosarajuImpl(6);
         g.addEdge(0,1);
         g.addEdge(1,2);
         g.addEdge(2,0);
         g.addEdge(2,3);
         g.addEdge(3,4);
         g.addEdge(4,5);
-        g.addEdge(5,3);
+        g.addEdge(5,3);*/
+        KosarajuImpl g=new KosarajuImpl(5);
+        g.addEdge(0,1);
+        g.addEdge(1,2);
+        g.addEdge(2,0);
+        g.addEdge(1,3);
+        g.addEdge(3,4);
         g.findSCC();
     }
 }
