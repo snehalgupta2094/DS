ec537e163eaec8c977a16ce388b1d4ebddd6baab
guptasn <guptasn@vmware.com>
2022-10-07 12:51:40 +0530

    dp

--- src/DP/MicCostTickets.java
+++ src/DP/MicCostTickets.java
@@ -1,6 +1,7 @@
 package DP;
 
 import java.util.Arrays;
+import java.util.Queue;
 
 public class MicCostTickets {
     //Recursion
@@ -46,7 +47,35 @@ public class MicCostTickets {
         dp[index]= Math.min(option1,Math.min(option2,option3));
         return dp[index];
     }
+    //tabulation
+    public static int solve3(int[]days, int costs[], int n){
+        int dp[]=new int[n+1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[n]=0;
+        for(int k=n-1;k>=0;k--){
+            //1 day pass
+            int op1=costs[0]+dp[k+1];
+            //7 day pass
+            int i;
+            for(i=k;i<n && days[i]<days[k]+7;i++);
+            int op2=costs[1]+dp[i];
+            //30 day pass
+            for(i=k;i<n && days[i]<days[k]+30;i++);
+            int op3=costs[2]+dp[i];
 
+            dp[k]=Math.min(op1,Math.min(op2,op3));
+        }
+    return dp[0];
+    }
+    //space optimization
+    public static int solve4(int[]days, int costs[], int index, int n){
+
+        return 0;
+    }
+    static class Pair<K,V>{
+        K first;
+        V second;
+    }
     public static void main(String[] args) {
          int days[]={1,4,6,7,8,20};
          int costs[]={2,7,15};
@@ -55,5 +84,6 @@ public class MicCostTickets {
         int dp[]=new int[n];
         Arrays.fill(dp, -1);
         System.out.println(solve2(days,costs,0,n,dp));
+        System.out.println(solve3(days,costs,n));
     }
 }
