3a288e0a7898ad611acfd54206210b1cde013a95
guptasn <guptasn@vmware.com>
2023-07-30 13:31:50 +0530

    neetcode

--- src/Arrays/GroupAnagrams.java
+++ src/Arrays/GroupAnagrams.java
@@ -9,19 +9,22 @@ import java.util.Map;
 public class GroupAnagrams {
 
     public static void main(String[] args) {
-        //Step1: sort each word
         String strs[]={"eat","tea","tan","ate","nat","bat"};
         HashMap<String, ArrayList<String>> groupAnagramsMap=new HashMap<>();
         for(String anagram: strs){
             char[] anagramArray=anagram.toCharArray();
             Arrays.sort(anagramArray);
-            System.out.println(String.valueOf(anagramArray));
             ArrayList<String> group=groupAnagramsMap.getOrDefault(String.valueOf(anagramArray),new ArrayList<>());
             group.add(anagram);
             groupAnagramsMap.put(String.valueOf(anagramArray),group);
         }
+        System.out.println("===Print map====");
         for (Map.Entry<String, ArrayList<String>> entry : groupAnagramsMap.entrySet()) {
             System.out.println(entry.getKey() + entry.getValue());
         }
+        System.out.println("===Final answer===");
+        List<List<String>> listOfList=new ArrayList<>(groupAnagramsMap.values());
+        System.out.println(listOfList);
+
     }
 }
