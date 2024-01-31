ebc93d787be3d9b6a99f57ec8030c410af8f7838
https://github.com/snehalgupta2094 <snehalgupta2094@gmail.com>
2024-01-31 10:57:23 +0530

    karat

--- src/Interviews/karat/LongestRepeatingPattern.java
+++ src/Interviews/karat/LongestRepeatingPattern.java
@@ -11,23 +11,22 @@ User2: ["hi", "stop", "leetcode", "start", "end", "bye"]
 Output: ["leetcode", "start", "end"]
 * */
 public class LongestRepeatingPattern {
-
+    static int maxLength = 0;
     static List<String> outputList = new ArrayList<>();
     public static void main(String[] args) {
-        //String[] User1 = {"hi", "bye", "hello", "leetcode", "start", "end"};
-        //String[] User2 = {"hi", "stop", "leetcode", "start", "end", "bye"};
+        String[] User1 = {"hi", "bye", "hello", "leetcode", "start", "end"};
+        String[] User2 = {"hi", "stop", "leetcode", "start", "end", "bye"};
 
-        String[] User1 = {"hi", "stop","bye", "hello", "leetcode", "start", "end", "snehal"};
-        String[] User2 = {"hi", "stop", "bye", "snehal","leetcode","abc", "start", "end", "bye"};
+      //  String[] User1 = {"hi", "bye", "hello", "leetcode", "start", "end", "snehal"};
+       // String[] User2 = {"hi", "stop", "bye", "snehal","leetcode","abc", "start", "end", "bye"};
 
-        int[] maxLength = new int[1];
-        findLongestPattern(0, 0 ,User1, User2, new ArrayList<>(), maxLength);
+        findLongestPattern(0, 0 ,User1, User2, new ArrayList<>());
 
-        System.out.println(maxLength[0]);
+        System.out.println(maxLength);
         System.out.println(outputList);
     }
 
-    public static int findLongestPattern(int i, int j, String[] User1, String[] User2, List<String> temp, int[] maxLength){
+    public static int findLongestPattern(int i, int j, String[] User1, String[] User2, List<String> temp){
         if(i >= User1.length || j >= User2.length){
             return 0;
         }
@@ -35,18 +34,16 @@ public class LongestRepeatingPattern {
         int length = 0;
         if(User1[i].equals(User2[j])){
             temp.add(User1[i]);
-            length = findLongestPattern(i + 1, j + 1, User1, User2, temp, maxLength) + 1;
-            if(length > maxLength[0]){
-                maxLength[0] = length;
+            length = findLongestPattern(i + 1, j + 1, User1, User2, temp) + 1;
+            if(length > maxLength){
+                maxLength = length;
                 outputList = temp;
             }
         }
 
-        findLongestPattern(i + 1, j , User1, User2, new ArrayList<>(), maxLength);
-        findLongestPattern(i , j + 1, User1, User2, new ArrayList<>(), maxLength);
+        findLongestPattern(i + 1, j , User1, User2, new ArrayList<>());
+        findLongestPattern(i , j + 1, User1, User2, new ArrayList<>());
         return length;
     }
-
-
 
 }
