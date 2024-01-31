13b2755279f6b26d33e1e71b4e0da3f3cc82e3d8
https://github.com/snehalgupta2094 <snehalgupta2094@gmail.com>
2024-01-31 12:34:16 +0530

    karat

--- src/Interviews/karat/WordInPattern.java
+++ src/Interviews/karat/WordInPattern.java
@@ -16,5 +16,43 @@ public class WordInPattern {
 
     public static void main(String[] args) {
      String[] words = {"cat", "dada", "breath" , "taking", "doll"};
+     String pattern1 = "drctkla";
+     String pattern2 = "rdatlbohe";
+
+     String output1 = findWordInPattern(words, pattern1);
+     String output2 = findWordInPattern(words, pattern2);
+
+     System.out.println(output1);
+     System.out.println(output2);
+    }
+
+    public static String findWordInPattern(String[] words, String pattern){
+        int[] patternArray = new int[26];
+
+        for(int index = 0; index < pattern.length(); index++){
+            patternArray[pattern.charAt(index) - 'a']++;
+        }
+
+        for(String word : words){
+            if(isValid(word, patternArray)){
+                return word;
+            }
+        }
+        return "";
+    }
+
+    public static boolean isValid(String word, int[] patternArray){
+        int[] wordArray = new int[26];
+
+
+        for(int index = 0 ;index < word.length(); index++){
+            int wordIndex = word.charAt(index) - 'a';
+            wordArray[wordIndex]++;
+
+            if(patternArray[wordIndex] < wordArray[wordIndex]) {
+                return false;
+            }
+        }
+        return true;
     }
 }
