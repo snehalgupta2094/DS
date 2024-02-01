2199c4fae7a512be255023191bde2d3eb4731aab
https://github.com/snehalgupta2094 <snehalgupta2094@gmail.com>
2024-02-01 07:10:22 +0530

    karat

--- src/Interviews/karat/FindRectangles.java
+++ src/Interviews/karat/FindRectangles.java
@@ -1,4 +1,8 @@
 package Interviews.karat;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /*
 * Give a matrix of 1's and 0's. For example, matrix = [
 ["1","1","1","1","1"],
@@ -23,5 +27,63 @@ matrix = [
 I told them I would check for the surrounding 1 but I didnt have time to finish the second round. Any insight on how to solve ? Thank you!
 * */
 public class FindRectangles {
+
+    public static void main(String[] args) {
+        String[][] matrix = {
+                {"1", "1", "1", "1", "1"},
+                {"1", "1", "0", "0", "1"},
+                {"1", "1", "0", "0", "1"},
+                {"1", "1", "1", "1", "1"}
+        };
+        findAllRectangles(matrix);
+       String matrix1[][] = {
+                {"0", "1", "1", "1", "1"},
+                {"1", "1", "0", "0", "1"},
+                {"0", "1", "0", "0", "1"},
+                {"0", "1", "1", "1", "1"},
+                {"1", "0", "1", "1", "1"}
+        };
+        findAllRectangles(matrix1);
+    }
+
+    public static void findAllRectangles(String[][] matrix){
+        List<Integer> startArray = new ArrayList<>();
+        List<Integer> endArray = new ArrayList<>();
+        boolean[][] visited = new boolean[matrix.length][matrix[0].length];
+
+        for(int row = 0; row < matrix.length; row++){
+            int[] start = new int[1];
+            int[] end = new int[1];
+            for(int col = 0; col < matrix[row].length; col++){
+                if(!visited[row][col] && matrix[row][col].equals("0")){
+                    startArray.add(row);
+                    endArray.add(col);
+                    findRectangle(row, col, matrix, start, end, visited);
+                    startArray.add(start[0]);
+                    endArray.add(end[0]);
+                }
+            }
+        }
+        System.out.println(startArray);
+        System.out.println(endArray);
+    }
+
+    private static void findRectangle(int row, int col, String[][] matrix, int[] start, int[] end, boolean[][] visited) {
+        if(row<0 || col<0 || row>= matrix.length || col>= matrix[0].length || visited[row][col] || matrix[row][col] == "1"){
+            return;
+        }
+
+        visited[row][col] = true;
+        findRectangle(row + 1, col, matrix, start, end, visited);
+        findRectangle(row, col + 1, matrix, start, end, visited);
+        findRectangle(row - 1, col, matrix, start, end, visited);
+        findRectangle(row, col - 1, matrix, start, end, visited);
+
+        if(matrix[row][col].equals("0") && start[0] <= row && end[0] <= col){
+            start[0] = row;
+            end[0] = col;
+        }
+    }
+
 
 }
