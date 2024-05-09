0a844c70543e94795ef25326bfd36d88bac5db85
snehalgupta2094 <snehalgupta2094@gmail.com>
2024-05-09 09:58:26 +0530

    moved to interviews repo

--- src/Interviews/karat/WordSearch.java
+++
@@ -1,9 +0,0 @@
-package Interviews.karat;
-/*
-* https://leetcode.com/problems/word-search/ but have to also return position of where the word was found in the matrix and need to go only in right and down directions in the matrix. Word is guaranteed to be in the matrix.
-Multiple words are given for searching in the word search problem (2) but each letter in the matrix can only be used towards one word. Not completely sure if the objective was trying to match maximum words or returning the index of matching words (assuming all can be matched). Very few minutes were left for this so couldn't discuss much with the interviewer about this.
-* I think I only provided some brute force solution but not sure if it was correct.
-* */
-public class WordSearch {
-
-}
--- src/Interviews/karat/WordsInVocabulary.java
+++
@@ -1,8 +0,0 @@
-package Interviews.karat;
-/*
-* word v is said to derive from word w if v can be obtained by rearranging the letters in w and adding one more letter (e.g. two -> wout). Given a list of vocabulary and a word w, return all words in the vocabulary that can be derived from w (either direct derivation or indirect derivation, so there can be intermediate steps between w and v)
-Each word in the vocabulary has a value associated with it. Given (w, v), return the path whose sum of values are the largest.
-* */
-public class WordsInVocabulary {
-
-}
